cf300f3302e46bd43135964a58346373c0877a9f
emilyoun <71864992+emilyoun@users.noreply.github.com>
2020-12-11 11:47:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//songs
+function setHalfVolume() {
+	var myAudio = document.getElementById("audio");
+	myAudio.volume = 0.2;
+}
+
+let on_off = document.querySelector(".musicOn");
+
+on_off.onclick = function () {
+	if (audio.paused) {
+		audio.play();
+	}
+	else {
+		audio.pause();
+	}
+}
